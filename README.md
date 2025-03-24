234a96930f52f4e2403958ebab72f774ff9c6455
donnasaud <donnaalmadani@gmail.com>
2025-03-24 21:03:11 +0000

    saving files

---
+++ README.md
@@ -0,0 +1,13 @@
+# holbertonschool-printf
+
+This group project implements a custom version of the C standard library printf() function, supporting basic formatting capabilities.
+
+## Supported Specifiers
+- %c : Character
+- %s : String
+- %% : Percent sign
+
+## Prototype of this project
+```c
+int _printf(const char *format, ...);
+
---
+++ _printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: The format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+				return (-1);
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str++, 1);
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
+
