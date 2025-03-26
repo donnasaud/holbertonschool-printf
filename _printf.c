f651f78023549c892ac1192a75dc6d59664b29bf
donnasaud <donnaalmadani@gmail.com>
2025-03-26 07:25:57 +0000

    t

--- _printf.c
+++ _printf.c
@@ -8,23 +8,21 @@
  */
 int handle_format(char format, va_list args)
 {
-    if (format == 'c')
-        return (print_char(args));
-    else if (format == 's')
-        return (print_string(args));
-    else if (format == 'd' || format == 'i')
-        return (print_number(args));
-    else if (format == '%')
-    {
-        write(1, "%", 1);
-        return (1);
-    }
-    else
-    {
-        write(1, "%", 1);
-        write(1, &format, 1);
-        return (2);
-    }
+	if (format == 'c')
+		return (print_char(args));
+	else if (format == 's')
+		return (print_string(args));
+	else if (format == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	else
+	{
+		write(1, "%", 1);
+		write(1, &format, 1);
+		return (2);
+	}
 }
 
 /**
@@ -34,31 +32,31 @@ int handle_format(char format, va_list args)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
+	va_list args;
+	int i = 0, count = 0;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (!format[i])
-                return (-1);
-            count += handle_format(format[i], args);
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+				return (-1);
+			count += handle_format(format[i], args);
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
